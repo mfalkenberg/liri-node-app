ec87a5ca5a250f4aab0daf30cb58b2c67be917cf
Maggie Falkenberg <mf1306@yahoo.com>
2018-02-11 15:56:25 -0800

    creaded files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<YX3S0klRSxCC9Hq1TXCDydAxL>',
+  consumer_secret: '<jSmyrzm6WRd1zJfmlzKPIpYVdjQezLeewmPheRpT0WgTxjOnVh>',
+  access_token_key: '<psQ1AcqVnsKnvQpkQmfzO5rNn>',
+  access_token_secret: '<Gde2lwy5uLC6Vmfyvgw4HsFNSb9i5ILdZtr2jfJaiI3cU7Iw3C>',
+}
+
+module.exports = twitterKeys;
